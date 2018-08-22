rowhit/go-ipfs

coreapi: update dht imports after rebase
License: MIT
Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

core/coreapi/dht_test.go
@@ -8,8 +8,8 @@ import (
 
 	coreapi "github.com/ipfs/go-ipfs/core/coreapi"
 
-	peer "gx/ipfs/QmZoWKhxUmZ2seW4BzX6fJkNR8hh9PsGModr7q171yq2SS/go-libp2p-peer"
-	blocks "gx/ipfs/Qmej7nf81hi2x2tvjRBF3mcp74sQyuDH4VMYDGd1YtXjb2/go-block-format"
+	peer "gx/ipfs/QmdVrMn1LhB4ybb8hMVaMLXnA8XRSewMnK6YqXKXoTcRvN/go-libp2p-peer"
+	blocks "gx/ipfs/QmVzK524a2VWLqyvtBeiHKsUAWYgeAk4DBeZoY7vpNPNRx/go-block-format"
 )
 
 func TestDhtFindPeer(t *testing.T) {

qupengcheng/go-udfs

core/coreapi/dht_test.go
@@ -8,8 +8,8 @@ import (
 
 	coreapi "github.com/ipfs/go-ipfs/core/coreapi"
 
-	peer "gx/ipfs/QmZoWKhxUmZ2seW4BzX6fJkNR8hh9PsGModr7q171yq2SS/go-libp2p-peer"
-	blocks "gx/ipfs/Qmej7nf81hi2x2tvjRBF3mcp74sQyuDH4VMYDGd1YtXjb2/go-block-format"
+	blocks "gx/ipfs/QmR54CzE4UcdFAZDehj6HFyy3eSHhVsJUpjfnhCmscuStS/go-block-format"
+	peer "gx/ipfs/QmdVrMn1LhB4ybb8hMVaMLXnA8XRSewMnK6YqXKXoTcRvN/go-libp2p-peer"
 )
 
 func TestDhtFindPeer(t *testing.T) {