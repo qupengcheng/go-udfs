8305d600f46f4f2cb23a0dd133b08ec227289137
danny <2528273456@qq.com>
2018-08-24 11:23:17 +0800

    feat: first finish the backup command

--- cmd/ipfs/daemon.go
+++ cmd/ipfs/daemon.go
@@ -401,6 +401,9 @@ func daemonFunc(req *cmds.Request, re cmds.ResponseEmitter, env cmds.Environment
 	prometheus.MustRegister(&corehttp.IpfsNodeCollector{Node: node})
 
 	fmt.Printf("Daemon is ready\n")
+
+	commands.SetupBackupHandler(node)
+
 	// collect long-running errors and block for shutdown
 	// TODO(cryptix): our fuse currently doesnt follow this pattern for graceful shutdown
 	for err := range merge(apiErrc, gwErrc, gcErrc) {
